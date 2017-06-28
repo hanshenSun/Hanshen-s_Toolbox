0f841625aced1562f783c7ef8a0f5f75bd064ff7
hanshenSun <hanshensunw@gmail.com>
2017-06-27 22:03:33 -0400

    0627

---
+++ MyProject_0624/CurveDirectionUnifier.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MyProject_0624
+{
+    public class CurveDirectionUnifier : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public CurveDirectionUnifier()
+          : base("CurveDirectionUnifier", "CurveDirUnifier",
+              "Unifies all the input curves towards a similar direction within tolerance",
+              "HS_ToolBox", "GeoFix")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "Crv", "Curves for sorting", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Tolerance", "t", "Tolerance in degree for the unifier", GH_ParamAccess.item);
+
+
+
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Sorted Curves", "Crv", "Sorted Curves", GH_ParamAccess.list);
+            pManager.AddVectorParameter("CurveTangent", "t", "Vector at middle", GH_ParamAccess.item);
+
+
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            //initial variable
+            List<Curve> originCrvs = new List<Curve>();
+            double flipTolerance = 0.0;
+
+            //calculation variable
+
+
+
+
+
+            if (!DA.GetDataList(0, originCrvs)) return;
+            if (!DA.GetData(1, ref flipTolerance)) return;
+            
+
+            if (originCrvs.Count<= 1) {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Need more than one curve for sorting");
+
+                return;
+            }
+
+
+            if (flipTolerance < 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Tolerance cannot be less than 0");
+
+                return;
+                
+            }
+
+
+            //Now let the coding game begin!!!
+            //Yayyyyyy
+
+
+            Curve firstCurve = originCrvs[0];
+            Vector3d firstVector = firstCurve.TangentAt(flipTolerance);
+
+            /*
+
+            foreach (Curve curvetoSort in originCrvs)
+            {
+
+
+            }
+            */
+
+
+            DA.SetData(1, firstVector);
+
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("44239989-3646-462f-883e-3ed15d0b279f"); }
+        }
+    }
+}
---
+++ MyProject_0624/MyProject_0624Info.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace MyProject_0624
+{
+    public class MyProject_0624Info : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "MyProject0624";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("609fa026-186a-4f41-9f5a-28a1f1c5cf67");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
