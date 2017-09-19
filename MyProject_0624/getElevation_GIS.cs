59a5a2605cd058dc7a7cf26d07930ed0498e150b
hanshenSun <hanshensunw@gmail.com>
2017-09-19 11:51:57 -0400

    some work on the gis tool

--- MyProject_0624/getElevation_GIS.cs
+++ MyProject_0624/getElevation_GIS.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
@@ -23,9 +27,11 @@ namespace MyProject_0624
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddNumberParameter("Lat", "Latitude", "Latitude", GH_ParamAccess.item);
-            pManager.AddNumberParameter("Lon", "Longitude", "Longitude", GH_ParamAccess.item);
+            pManager.AddTextParameter("Lat", "Latitude", "Latitude", GH_ParamAccess.item);
+            pManager.AddTextParameter("Lon", "Longitude", "Longitude", GH_ParamAccess.item);
             pManager.AddIntegerParameter("Res", "Resolution", "Resolution", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+            pManager[1].Optional = true;
         }
 
         /// <summary>
@@ -42,8 +48,8 @@ namespace MyProject_0624
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            double inputLatitude = 0.0;
-            double inputLongitude = 0.0;
+            string inputLatitude = "36.578581,-118.291994";
+            string inputLongitude = "36.23998,-116.83171";
             int resCount = 10;
 
             DA.GetData(0, ref inputLatitude);
@@ -55,7 +61,25 @@ namespace MyProject_0624
             string actualUrl = ELEVATION_BASE_URL + path + "&samples=" + resCount;
 
 
+            //response = simplejson.load(urllib.urlopen(url))
 
+            WebClient wc = new WebClient();
+
+            var js = wc.DownloadString(actualUrl);
+            JObject json = JObject.Parse(js);
+            //JsonConvert.DeserializeObject<>(js);
+
+
+            List<string> elevationData = new List<string>();
+
+            
+            
+            foreach (var resultset in json["results"])
+            {
+                elevationData.Add(resultset["elevation"].ToString());
+            }
+            
+            DA.SetDataList(0, elevationData);
         }
 
         /// <summary>
